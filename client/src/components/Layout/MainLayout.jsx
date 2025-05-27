1829579fba654ada99057a90e2f956c561b0ad6c
Niladri Chakraborty <niladrichakraborty16@gmail.com>
2025-05-27 18:53:51 +0530

    Components: New Navbar Added

--- client/src/components/Layout/MainLayout.jsx
+++ client/src/components/Layout/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import NavbarV2 from '../Navbar/NavbarV2';
+import NavbarV3 from '../Navbar/NavbarV3';
 
 const MainLayout = ({ isSearchOpen, setIsSearchOpen, searchInputRef }) => {
     return (
         <div>
-            <Navbar isSearchOpen={isSearchOpen} setIsSearchOpen={setIsSearchOpen} searchInputRef={searchInputRef} />
+            <NavbarV3 />
+            {/* <NavbarV2 /> */}
+            {/* <Navbar isSearchOpen={isSearchOpen} setIsSearchOpen={setIsSearchOpen} searchInputRef={searchInputRef} /> */}
             <Outlet />
             <Footer />
         </div>
