ea0ec1c12915324fb6fa13c8e0ec6efcc57fb58a
Niladri Chakraborty <niladrichakraborty16@gmail.com>
2025-07-09 18:53:51 +0530

    Elastic: Deleted unrelated files

--- server/src/main/java/com/ecommerce/repository/ProductESRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.model.ProductES;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ProductESRepository extends ElasticsearchRepository<ProductES, Long> {
-}
