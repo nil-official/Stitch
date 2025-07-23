a02075e25f3cf42bd48ed6522c14951b9c8c8953
Niladri Chakraborty <niladrichakraborty16@gmail.com>
2025-07-23 13:50:56 +0530

    ScrollToTop added, Resolves #13

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,26 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import AuthGuard from './utils/AuthGuard';
 import UserRoutes from './routes/UserRoutes';
 import AdminRoutesV2 from './routes/AdminRoutesV2';
+import { loadUser } from './redux/auth/action';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadUser());
+  }, []);
+
   return (
     <AuthGuard>
+      <ScrollToTop />
       <Routes>
-
         <Route path="/*" element={<UserRoutes />} />
         <Route path="/admin/*" element={<AdminRoutesV2 />} />
-
       </Routes>
     </AuthGuard>
   );
