c419ba12dd87dd939cc66cb1c2415645a6ee281f
Niladri Chakraborty <niladrichakraborty16@gmail.com>
2025-07-30 16:40:05 +0530

    Analytics: Vercel Analytics added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import Providers from './Providers.jsx';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 createRoot(document.getElementById('root')).render(
   <Providers>
